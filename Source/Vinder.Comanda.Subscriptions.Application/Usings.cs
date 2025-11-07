18a19799cd22ebd294c3aafd66815171e002cfe3
Richard Garcia <code.richardy@gmail.com>
2025-11-06 21:56:13 -0300

    this commit introduces a usings.cs file to define global using directives for domain, application, and internal essentials namespaces, simplifying imports across the project

---
+++ Source/Vinder.Comanda.Subscriptions.Application/Usings.cs
@@ -0,0 +1,26 @@
+global using System.Text.Json.Serialization;
+
+global using Vinder.Comanda.Subscriptions.Domain.Entities;
+global using Vinder.Comanda.Subscriptions.Domain.Entities.Enums;
+global using Vinder.Comanda.Subscriptions.Domain.Concepts;
+global using Vinder.Comanda.Subscriptions.Domain.Errors;
+global using Vinder.Comanda.Subscriptions.Domain.Repositories;
+global using Vinder.Comanda.Subscriptions.Domain.Filtering;
+
+global using Vinder.Comanda.Subscriptions.Application.Mappers;
+global using Vinder.Comanda.Subscriptions.Application.Gateways;
+global using Vinder.Comanda.Subscriptions.Application.Payloads;
+global using Vinder.Comanda.Subscriptions.Application.Payloads.Traceability;
+global using Vinder.Comanda.Subscriptions.Application.Payloads.Subscription;
+
+
+global using Vinder.Internal.Essentials.Contracts;
+global using Vinder.Internal.Essentials.Patterns;
+global using Vinder.Internal.Essentials.Entities;
+global using Vinder.Internal.Essentials.Filters;
+global using Vinder.Internal.Essentials.Primitives;
+global using Vinder.Internal.Essentials.Utilities;
+global using Vinder.Internal.Essentials.Extensions;
+
+global using Vinder.Dispatcher.Contracts;
+global using FluentValidation;
