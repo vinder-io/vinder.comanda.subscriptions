917f551250eef2a0e7c0e86974af1ee13fd68029
Richard Garcia <code.richardy@gmail.com>
2026-01-14 23:02:21 -0300

    this commit removes permissions.cs and its activity and subscription permission constants from the project

--- Source/Vinder.Comanda.Subscriptions.CrossCutting/Constants/Permissions.cs
+++
@@ -1,9 +0,0 @@
-namespace Vinder.Comanda.Subscriptions.CrossCutting.Constants;
-
-public static class Permissions
-{
-    public const string ViewActivities = "permissions.activities.view";
-    public const string ViewSubscriptions = "permissions.subscriptions.view";
-    public const string Subscribe = "permissions.subscriptions.subscribe";
-    public const string CancelSubscription = "permissions.subscriptions.cancel";
-}
