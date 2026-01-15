e6f1cbbdc42100eac8745afd28f23c6a35e1d425
Richard Garcia <code.richardy@gmail.com>
2026-01-14 23:03:46 -0300

    this commit refactors usespecification to accept iwebhostenvironment, enabling oauth2 and openapi configuration per environment, adds options to hide models and search, and reorganizes openapi setup with dynamic token url and transformers for clarity

--- Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/SpecificationExtension.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/SpecificationExtension.cs
@@ -2,16 +2,39 @@ namespace Vinder.Comanda.Subscriptions.WebApi.Extensions;
 
 public static class SpecificationsExtension
 {
-    public static void UseSpecification(this IEndpointRouteBuilder app)
+    public static void UseSpecification(this IEndpointRouteBuilder app, IWebHostEnvironment environment)
     {
         app.MapScalarApiReference(options =>
         {
             options.DarkMode = false;
             options.HideDarkModeToggle = true;
             options.HideClientButton = true;
+            options.HideModels = true;
+            options.HideSearch = true;
 
             options.WithTitle("Vinder Comanda Subscriptions API");
             options.WithClassicLayout();
+
+            if (environment.IsProduction())
+            {
+                options.AddPreferredSecuritySchemes(SecuritySchemes.OAuth2);
+                options.AddClientCredentialsFlow(SecuritySchemes.OAuth2, flow =>
+                {
+                    flow.WithCredentialsLocation(CredentialsLocation.Body);
+                });
+
+                return;
+            }
+
+            var settings = app.ServiceProvider.GetService<ISettings>()!;
+
+            options.AddPreferredSecuritySchemes(SecuritySchemes.OAuth2);
+            options.AddClientCredentialsFlow(SecuritySchemes.OAuth2, flow =>
+            {
+                flow.ClientId = settings.Federation.ClientId;
+                flow.ClientSecret = settings.Federation.ClientSecret;
+                flow.WithCredentialsLocation(CredentialsLocation.Body);
+            });
         });
     }
 }
--- Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/WebInfrastructureExtension.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/WebInfrastructureExtension.cs
@@ -1,3 +1,5 @@
+using Stripe.Tax;
+
 namespace Vinder.Comanda.Subscriptions.WebApi.Extensions;
 
 [ExcludeFromCodeCoverage(Justification = "contains only web infrastructure configuration with no business logic.")]
@@ -9,11 +11,36 @@ public static class WebInfrastructureExtension
         services.AddHttpContextAccessor();
         services.AddEndpointsApiExplorer();
         services.AddCorsConfiguration();
-        services.AddOpenApi();
         services.AddIdentityServer();
         services.AddFluentValidationAutoValidation(options =>
         {
             options.DisableDataAnnotationsValidation = true;
+        });
+
+        var provider = services.BuildServiceProvider();
+        var settings = provider.GetRequiredService<ISettings>();
+
+        services.AddOpenApi(options =>
+        {
+            options.AddScalarTransformers();
+            options.AddDocumentTransformer((document, _, _) =>
+            {
+                document.Components ??= new Microsoft.OpenApi.Models.OpenApiComponents();
+                document.Components.SecuritySchemes[SecuritySchemes.OAuth2] =
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
+                        Flows = new Microsoft.OpenApi.Models.OpenApiOAuthFlows
+                        {
+                            ClientCredentials = new Microsoft.OpenApi.Models.OpenApiOAuthFlow
+                            {
+                                TokenUrl = new Uri(settings.Federation.BaseUrl + "/api/v1/openid/connect/token")
+                            }
+                        }
+                    };
+
+                return Task.CompletedTask;
+            });
         });
     }
 }
