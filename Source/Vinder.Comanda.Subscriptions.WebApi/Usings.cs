1dd6c296cb480de1f874f6bcc9da2a6cb84cc093
Richard Garcia <code.richardy@gmail.com>
2026-01-14 23:00:52 -0300

    this commit refactors usings to centralize constants in webapi by removing crosscutting.constants and crosscutting.configurations and adding webapi.constants reference

--- Source/Vinder.Comanda.Subscriptions.WebApi/Usings.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Usings.cs
@@ -4,15 +4,14 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Authorization;
 
 global using Vinder.Comanda.Subscriptions.WebApi.Extensions;
+global using Vinder.Comanda.Subscriptions.WebApi.Constants;
 global using Vinder.Comanda.Subscriptions.Domain.Errors;
 
 global using Vinder.Comanda.Subscriptions.Application.Payloads.Traceability;
 global using Vinder.Comanda.Subscriptions.Application.Payloads.Subscription;
 
 global using Vinder.Comanda.Subscriptions.Infrastructure.IoC.Extensions;
-
 global using Vinder.Comanda.Subscriptions.CrossCutting.Configurations;
-global using Vinder.Comanda.Subscriptions.CrossCutting.Constants;
 
 global using Vinder.Dispatcher.Contracts;
 global using Vinder.IdentityProvider.Sdk.Extensions;
