e27f02934772c55ca4656c9bfe52c3e0387478ee
Richard Garcia <code.richardy@gmail.com>
2026-01-29 19:21:44 -0300

    feat(#2): this commit introduces observability extension to configure serilog for logging in the application

---
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/ObservabilityExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Vinder.Comanda.Subscriptions.WebApi.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "contains only http pipeline configuration with no business logic.")]
+public static class ObservabilityExtension
+{
+    public static void AddObservability(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, services, logger) =>
+        {
+            var settings = services.GetRequiredService<ISettings>();
+
+            logger
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq(settings.Observability.SeqServerUrl);
+        });
+    }
+}
