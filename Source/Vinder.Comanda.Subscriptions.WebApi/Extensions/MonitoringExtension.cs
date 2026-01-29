b46f31d26f6c1d339a5879801d37d8de36d40b78
Richard Garcia <code.richardy@gmail.com>
2026-01-29 19:22:36 -0300

    feat(#2): this commit introduces monitoring extension for sentry observability integration

---
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/MonitoringExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Vinder.Comanda.Subscriptions.WebApi.Extensions;
+
+public static class MonitoringExtension
+{
+    public static void AddMonitoring(this WebApplicationBuilder builder)
+    {
+        var settings = builder.Services
+            .BuildServiceProvider()
+            .GetRequiredService<ISettings>();
+
+        // https://docs.sentry.io/platforms/dotnet/
+        builder.WebHost.UseSentry(options =>
+        {
+            options.Dsn = settings.Observability.SentryDsn;
+            options.Environment = builder.Environment.EnvironmentName;
+            options.TracesSampleRate = 1.0;
+            options.Debug = true;
+        });
+    }
+}
