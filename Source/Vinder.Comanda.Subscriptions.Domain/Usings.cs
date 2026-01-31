d0440e3f0dff11339e9e299e2b6e49c20b8b7d69
Richard Garcia <code.richardy@gmail.com>
2026-01-31 11:46:11 -0300

    refactor: update global usings for improved organization and clarity

--- Source/Vinder.Comanda.Subscriptions.Domain/Usings.cs
+++ Source/Vinder.Comanda.Subscriptions.Domain/Usings.cs
@@ -1,10 +1,10 @@
-global using Vinder.Internal.Essentials.Entities;
+global using Vinder.Internal.Essentials.Aggregates;
 global using Vinder.Internal.Essentials.Contracts;
-global using Vinder.Internal.Essentials.Primitives;
+global using Vinder.Internal.Essentials.Concepts;
 global using Vinder.Internal.Essentials.Patterns;
 
-global using Vinder.Internal.Essentials.Filters;
-global using Vinder.Internal.Essentials.Filters.Builders;
+global using Vinder.Internal.Essentials.Filtering;
+global using Vinder.Internal.Essentials.Filtering.Builders;
 
 global using Vinder.Comanda.Subscriptions.Domain.Concepts;
 global using Vinder.Comanda.Subscriptions.Domain.Entities;
