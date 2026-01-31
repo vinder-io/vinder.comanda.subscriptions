6b281f7381c9ef308f730e1c4c3c88ec7c7f81d3
Richard Garcia <code.richardy@gmail.com>
2026-01-31 11:44:19 -0300

    refactor: simplify AddWebComposition method by removing unused parameters and redundant code

--- Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/WebInfrastructureExtension.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Extensions/WebInfrastructureExtension.cs
@@ -1,46 +1,30 @@
-using Stripe.Tax;
-
 namespace Vinder.Comanda.Subscriptions.WebApi.Extensions;
 
 [ExcludeFromCodeCoverage(Justification = "contains only web infrastructure configuration with no business logic.")]
 public static class WebInfrastructureExtension
 {
-    public static void AddWebComposition(this IServiceCollection services, IWebHostEnvironment environment)
+    public static void AddWebComposition(this IServiceCollection services)
     {
+        var provider = services.BuildServiceProvider();
+        var settings = provider.GetRequiredService<ISettings>();
+
         services.AddControllers();
         services.AddHttpContextAccessor();
         services.AddEndpointsApiExplorer();
         services.AddCorsConfiguration();
-        services.AddIdentityServer();
+
         services.AddFluentValidationAutoValidation(options =>
         {
             options.DisableDataAnnotationsValidation = true;
         });
 
-        var provider = services.BuildServiceProvider();
-        var settings = provider.GetRequiredService<ISettings>();
-
-        services.AddOpenApi(options =>
+        services.AddOpenApiSpecification();
+        services.AddFederation(options =>
         {
-            options.AddScalarTransformers();
-            options.AddDocumentTransformer((document, _, _) =>
-            {
-                document.Components ??= new Microsoft.OpenApi.Models.OpenApiComponents();
-                document.Components.SecuritySchemes[SecuritySchemes.OAuth2] =
-                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                    {
-                        Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
-                        Flows = new Microsoft.OpenApi.Models.OpenApiOAuthFlows
-                        {
-                            ClientCredentials = new Microsoft.OpenApi.Models.OpenApiOAuthFlow
-                            {
-                                TokenUrl = new Uri(settings.Federation.BaseUrl + "/api/v1/openid/connect/token")
-                            }
-                        }
-                    };
-
-                return Task.CompletedTask;
-            });
+            options.BaseUrl = settings.Federation.BaseUrl;
+            options.ClientId = settings.Federation.ClientId;
+            options.Tenant = settings.Federation.Tenant;
+            options.ClientSecret = settings.Federation.ClientSecret;
         });
     }
 }
