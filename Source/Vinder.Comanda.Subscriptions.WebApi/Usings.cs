9c0e54ada0f554e983cdc7bf9a104c530f1672a7
Richard Garcia <code.richardy@gmail.com>
2026-01-31 11:43:18 -0300

    fix: update using directives for microsoft.openapi.models and correct vinder.federation.sdk.extensions reference

--- Source/Vinder.Comanda.Subscriptions.WebApi/Usings.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Usings.cs
@@ -2,6 +2,7 @@ global using System.Diagnostics.CodeAnalysis;
 
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.OpenApi.Models;
 
 global using Vinder.Comanda.Subscriptions.WebApi.Extensions;
 global using Vinder.Comanda.Subscriptions.WebApi.Constants;
@@ -14,7 +15,7 @@ global using Vinder.Comanda.Subscriptions.Infrastructure.IoC.Extensions;
 global using Vinder.Comanda.Subscriptions.CrossCutting.Configurations;
 
 global using Vinder.Dispatcher.Contracts;
-global using Vinder.IdentityProvider.Sdk.Extensions;
+global using Vinder.Federation.Sdk.Extensions;
 
 global using Scalar.AspNetCore;
 global using Serilog;
