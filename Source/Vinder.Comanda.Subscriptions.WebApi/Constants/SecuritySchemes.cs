dda3932598f8a0fc39777108a19a223ea24ba6c3
Richard Garcia <code.richardy@gmail.com>
2026-01-31 11:43:32 -0300

    fix: this commit introduces bearer constant to SecuritySchemes for improved clarity

--- Source/Vinder.Comanda.Subscriptions.WebApi/Constants/SecuritySchemes.cs
+++ Source/Vinder.Comanda.Subscriptions.WebApi/Constants/SecuritySchemes.cs
@@ -3,4 +3,5 @@ namespace Vinder.Comanda.Subscriptions.WebApi.Constants;
 public static class SecuritySchemes
 {
     public const string OAuth2 = "OAuth2";
+    public const string Bearer = "Bearer";
 }
